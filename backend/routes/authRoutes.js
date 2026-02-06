e77cdc9eddc9887bd1561883ae545a534fe78a06
allanjoseph01 <allanjoseph3005@gmail.com>
2026-02-06 21:55:44 +0530

    Switched storing photos from local to cloudinary

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { registerUser, loginUser, getUserProfile, updateUserProfile } = require("../controllers/authController");
 const { protect } = require("../middlewares/authMiddleware");
 const upload = require("../middlewares/uploadMiddleware");
+const cloudinary = require("../config/cloudinary");
 
 const router = express.Router();
 
@@ -10,12 +11,27 @@ router.post("/login",loginUser);
 router.get("/profile",protect , getUserProfile);
 router.put("/profile" , protect , updateUserProfile);
 
-router.post("/upload-image", upload.single("image"), (req, res) => {
+router.post("/upload-image", upload.single("image"), async (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: "No file uploaded" });
   }
-  const imageUrl = `${req.protocol}:/${req.get("host")}/uploads/${req.file.filename}`;
-  res.status(200).json({ imageUrl });
+
+  try {
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream({ folder: "uploads" }, (error, result) => {
+          if (error) reject(error);
+          else resolve(result);
+        })
+        .end(req.file.buffer);
+    });
+
+    res.status(200).json({
+      imageUrl: uploadResult.secure_url,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Image upload failed" });
+  }
 });
 
 module.exports = router;
